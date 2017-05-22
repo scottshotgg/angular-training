05657f32a05344bb28ee7dd6a4bcf9e1c0d5ec16
scottshotgg <scgaydos@gmail.com>
2017-05-22 14:56:52 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,39 @@
+var importMap = {
+	NgModule: 		'@angular/core',
+	BrowserModule: 	'@angular/platform-browser',
+	FormsModule: 	'@angular/forms',
+	AppComponent: 	'./app.component'
+};
+
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // NgModel
 
 import { AppComponent }  from './app.component';
 
+/*
+console.log('hi');
+
+Object.keys(importMap).map(function(item) {
+	console.log(item, importMap[item]);
+	//import { AppComponent }  from './app.component';
+});
+*/
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: 
+  [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  declarations: 
+  [
+    AppComponent
+  ],
+  bootstrap: 
+  [ 
+  	AppComponent 
+  ]
 })
 export class AppModule { }
